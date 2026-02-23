d54daaa8ad325f5fb6176be421d8f5b9c26510fb
Pranshi Patel <patelpranshi88@gmail.com>
2026-02-23 17:06:34 +0530

    [Pranshi Patel] : added UC4 Program

--- src/main/java/com/apps/quantitymeasurement/Length.java
+++ src/main/java/com/apps/quantitymeasurement/Length.java
@@ -7,6 +7,7 @@ public class Length {
 
  private final double value;
  private final LengthUnit unit;
+ 
 
  public Length(double value, LengthUnit unit) {
      if (unit == null) {
@@ -17,7 +18,7 @@ public class Length {
  }
 
  private double convertToBaseUnit() {
-     return this.value * unit.getConversionFactor();
+     return value * unit.getConversionFactor();
  }
 
  public boolean compare(Length other) {
@@ -30,26 +31,26 @@ public class Length {
      ) == 0;
  }
 
+ 
+ private double round(double value) {
+     return Math.round(value * 10000.0) / 10000.0;
+ }
+ 
  @Override
  public boolean equals(Object obj) {
 
-     if (this == obj)
-         return true;
-
-     if (obj == null || getClass() != obj.getClass())
-         return false;
+     if (this == obj) return true;
+     if (!(obj instanceof Length)) return false;
 
      Length other = (Length) obj;
 
-     return Double.compare(
-             this.convertToBaseUnit(),
-             other.convertToBaseUnit()
-     ) == 0;
+     return round(this.convertToBaseUnit())
+             == round(other.convertToBaseUnit());
  }
 
  @Override
  public int hashCode() {
-     return Objects.hash(convertToBaseUnit());
+     return Objects.hash(round(convertToBaseUnit()));
  }
 
  @Override
