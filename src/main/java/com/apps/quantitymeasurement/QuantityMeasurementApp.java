61717b485e5e79b78817abf46a3b51294f69e58c
Pranshi Patel <patelpranshi88@gmail.com>
2026-03-02 16:40:40 +0530

    [Pranshi Patel] : added UC9 Program

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -45,6 +45,59 @@ public class QuantityMeasurementApp {
                 "add(" + l1 + ", " + l2 + ", " + targetUnit + ") → " + result );
     }
 
+
+
+    // UC9 : Weight Class Methods 
+
+    public static boolean demonstrateWeightEquality(QuantityWeight w1, QuantityWeight w2) {
+        return w1.equals(w2);
+    }
+    
+    private static void checkWeightEquality(QuantityWeight w1, QuantityWeight w2) {
+        System.out.println(w1 + " and " + w2 +
+                " Equal: " + demonstrateWeightEquality(w1, w2));
+    }
+
+    // Weight conversion 
+    // Unit-to-Unit Conversion (Static style like Length.convert)
+    public static void demonstrateWeightConversion(double value, WeightUnit fromUnit, WeightUnit toUnit) {
+
+        QuantityWeight weight = new QuantityWeight(value, fromUnit);
+        QuantityWeight converted = weight.convertTo(toUnit);
+
+        System.out.println(value + " " + fromUnit +
+                " is " + converted.getValue() + " " + toUnit);
+    }
+
+
+    // QuantityWeight-to-QuantityWeight Conversion
+    public static void demonstrateWeightConversion(QuantityWeight weight, WeightUnit toUnit) {
+        System.out.println(weight + " converted to " + toUnit + " is " + weight.convertTo(toUnit));
+    }
+
+
+    // Weight Addition on Default Unit
+    public static void demonstrateWeightAddition(double value1, WeightUnit unit1, double value2, WeightUnit unit2) {
+        QuantityWeight w1 = new QuantityWeight(value1, unit1);
+        QuantityWeight w2 = new QuantityWeight(value2, unit2);
+        QuantityWeight result = w1.add(w2);
+        System.out.println(w1 + " + " + w2 + " = " + result);
+    }
+
+    // Weigth Addition on Target Unit
+    public static void demonstrateWeightAddition(double value1, WeightUnit unit1, double value2, WeightUnit unit2, WeightUnit targetUnit) {
+
+            QuantityWeight w1 = new QuantityWeight(value1, unit1);
+            QuantityWeight w2 = new QuantityWeight(value2, unit2);
+            QuantityWeight result = QuantityWeight.add(w1, w2, targetUnit);
+            System.out.println(
+            "add(" + w1 + ", " + w2 + ", " + targetUnit + ") → " + result
+            );
+    }
+
+
+
+
     public static void main(String[] args) {
 
         // Same Unit
@@ -81,5 +134,37 @@ public class QuantityMeasurementApp {
         demonstrateLengthAddition(2.54, LengthUnit.CENTIMETERS, 1.0, LengthUnit.INCHES, LengthUnit.CENTIMETERS);
         demonstrateLengthAddition(5.0, LengthUnit.FEET, 0.0, LengthUnit.INCHES, LengthUnit.YARDS);
         demonstrateLengthAddition(5.0, LengthUnit.FEET, -2.0, LengthUnit.FEET, LengthUnit.INCHES);
+
+        // UC9 : Weight Class Operations
+        
+        // Equality for Same Unit
+        checkWeightEquality( new QuantityWeight(1.0, WeightUnit.KILOGRAM), new QuantityWeight(1000.0, WeightUnit.GRAM));
+        checkWeightEquality(new QuantityWeight(2.0, WeightUnit.KILOGRAM), new QuantityWeight(2.0, WeightUnit.KILOGRAM));
+        checkWeightEquality( new QuantityWeight(500.0, WeightUnit.GRAM), new QuantityWeight(500.0, WeightUnit.GRAM));
+        checkWeightEquality( new QuantityWeight(3.0, WeightUnit.POUND), new QuantityWeight(3.0, WeightUnit.POUND));
+        
+        // Equality for Cross Unit
+        checkWeightEquality( new QuantityWeight(1.0, WeightUnit.KILOGRAM), new QuantityWeight(1000.0, WeightUnit.GRAM));
+        checkWeightEquality( new QuantityWeight(453.592, WeightUnit.GRAM), new QuantityWeight(1.0, WeightUnit.POUND));
+        
+        // Conversion
+        demonstrateWeightConversion(1.0, WeightUnit.KILOGRAM, WeightUnit.POUND);
+        demonstrateWeightConversion(1.0, WeightUnit.KILOGRAM, WeightUnit.GRAM);
+        demonstrateWeightConversion(1.0, WeightUnit.KILOGRAM, WeightUnit.POUND);
+        demonstrateWeightConversion(2000.0, WeightUnit.GRAM, WeightUnit.KILOGRAM);
+        demonstrateWeightConversion(500.0, WeightUnit.GRAM, WeightUnit.POUND);
+        demonstrateWeightConversion(2.0, WeightUnit.POUND, WeightUnit.KILOGRAM);
+        demonstrateWeightConversion(1.0, WeightUnit.POUND, WeightUnit.GRAM);
+
+        // Addition (implicit)
+        demonstrateWeightAddition(1.0, WeightUnit.KILOGRAM, 1000.0, WeightUnit.GRAM);
+        demonstrateWeightAddition( 2.0, WeightUnit.KILOGRAM, 3.0, WeightUnit.KILOGRAM);
+        demonstrateWeightAddition( 300.0, WeightUnit.GRAM, 200.0, WeightUnit.GRAM);
+
+        // Addition (explicit)
+        demonstrateWeightAddition(1.0, WeightUnit.KILOGRAM, 1000.0, WeightUnit.GRAM, WeightUnit.GRAM);
+        demonstrateWeightAddition( 1.0, WeightUnit.KILOGRAM, 1000.0, WeightUnit.GRAM, WeightUnit.GRAM);
+        demonstrateWeightAddition( 1.0, WeightUnit.POUND, 453.592, WeightUnit.GRAM, WeightUnit.POUND);
+        demonstrateWeightAddition(2.0, WeightUnit.KILOGRAM, 4.0, WeightUnit.POUND, WeightUnit.KILOGRAM);
     }
 }
