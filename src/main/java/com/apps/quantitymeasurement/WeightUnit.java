7f1a57e40301b4777a7be1f40ef4762f47a98cf1
Pranshi Patel <patelpranshi88@gmail.com>
2026-03-02 17:17:49 +0530

    [Pranshi Patel] : added UC14 Program

--- src/main/java/com/apps/quantitymeasurement/WeightUnit.java
+++ src/main/java/com/apps/quantitymeasurement/WeightUnit.java
@@ -7,29 +7,29 @@ public enum WeightUnit implements IMeasurable {
     TONNE(1000.0),
     POUND(0.453592);
 
-    private final double conversionFactor;
+    private final double toKilogramFactor;
 
-    WeightUnit(double conversionFactor) {
-        this.conversionFactor = conversionFactor;
-    }
-
-    @Override
-    public double getConversionFactor() {
-        return conversionFactor;
+    WeightUnit(double toKilogramFactor) {
+        this.toKilogramFactor = toKilogramFactor;
     }
 
     @Override
     public double convertToBaseUnit(double value) {
-        return value * conversionFactor;
+        return value * toKilogramFactor;
     }
 
     @Override
     public double convertFromBaseUnit(double baseValue) {
-        return baseValue / conversionFactor;
+        return baseValue / toKilogramFactor;
     }
 
     @Override
     public String getUnitName() {
         return this.name();
+    }
+
+    
+    public double getConversionFactor() {
+        return toKilogramFactor;
     }
 }
