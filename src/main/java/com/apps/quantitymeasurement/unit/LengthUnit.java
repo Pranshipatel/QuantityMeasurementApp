d303c0169dfbff1ad13efc4f2c80e7c4d28eb2c2
Pranshi Patel <patelpranshi88@gmail.com>
2026-03-13 11:49:41 +0530

    [Pranshi Patel] : added Unit Program

---
+++ src/main/java/com/apps/quantitymeasurement/unit/LengthUnit.java
@@ -0,0 +1,34 @@
+package com.apps.quantitymeasurement.unit;
+
+public enum LengthUnit implements IMeasurable{
+	FEET(1.0),
+	INCHES(1.0/12.0),
+	YARD(3.0),
+	CENTIMETERS(0.0328084);
+	
+	private double conversion;
+	
+	LengthUnit(double conversion){
+		this.conversion = conversion;
+	}
+	
+	@Override
+	public double getConversionFactor() {
+		return conversion;
+	}
+
+	@Override
+	public double convertToBaseUnit(double value) {
+		return ((value*this.getConversionFactor())*100.0)/100.0;
+	}
+
+	@Override
+	public double convertFromBaseUnit(double baseValue) {
+		return baseValue/this.getConversionFactor();
+	}
+
+	@Override
+	public String getUnitName() {
+		return LengthUnit.this.toString();
+	}
+}
---
+++ src/main/java/com/apps/quantitymeasurement/unit/WeightUnit.java
@@ -0,0 +1,36 @@
+package com.apps.quantitymeasurement.unit;
+
+public enum WeightUnit implements IMeasurable{
+	KILOGRAM(1.0),
+    // 1 Gram = 0.001 Kilograms
+    GRAM(0.001),
+    // 1 Pound = 0.453592 Kilograms
+    POUND(0.453592);
+	
+	private double conversion;
+	
+	WeightUnit(double conversion){
+		this.conversion = conversion;
+	}
+	
+	@Override
+	public double convertToBaseUnit(double value) {
+		return ((value*this.getConversionFactor())*100.0)/100.0;
+	}
+
+	@Override
+	public double getConversionFactor() {
+		return conversion;
+	}
+
+
+	@Override
+	public double convertFromBaseUnit(double baseValue) {
+		return baseValue/this.getConversionFactor();
+	}
+
+	@Override
+	public String getUnitName() {
+		return WeightUnit.this.toString();
+	}
+}
