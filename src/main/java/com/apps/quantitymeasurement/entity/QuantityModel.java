8e9c59f8989e5231d9784f2bf01847429dd35c9d
Pranshi Patel <patelpranshi88@gmail.com>
2026-03-17 11:22:13 +0530

    [Pranshi Patel] : added UC16 Program

--- src/main/java/com/apps/quantitymeasurement/entity/QuantityModel.java
+++ src/main/java/com/apps/quantitymeasurement/entity/QuantityModel.java
@@ -5,16 +5,19 @@ import com.apps.quantitymeasurement.unit.IMeasurable;
 public class QuantityModel<U extends IMeasurable> {
 	public double value;
 	public U unit;
-	
+
+	// Parameterized constructor
 	public QuantityModel(double value, U unit) {
 		this.value = value;
 		this.unit = unit;
 	}
 
+	// Method to get value
 	public double getValue() {
 		return value;
 	}
 
+	// Method to get unit
 	public U getUnit() {
 		return unit;
 	}
